0425ca84fcfdc6946e1769dc6df3a523b23f164d
Radium <2043869032@qq.com>
2023-01-04 22:48:22 +0800

    init

---
+++ bingchunk/binary_chunk.go
@@ -0,0 +1,21 @@
+package binchunk
+
+type header struct {
+	signature       [4]byte
+	version         byte
+	format          byte
+	luacData        [6]byte
+	cintSize        byte
+	sizetSize       byte
+	instructionSize byte
+	luaIntegerSize  byte
+	luaNumberSize   byte
+	luacInt         int64
+	luacNum         float64
+}
+
+type binaryChunk struct {
+	header
+	sizeUpvalues byte
+	mainFunc     *Prototype
+}
