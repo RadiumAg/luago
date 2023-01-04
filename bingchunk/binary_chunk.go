0a87c08b9f7e02ea66407b95d9c573f861472d24
Radium <2043869032@qq.com>
2023-01-04 22:59:02 +0800

    chore: commit

--- bingchunk/binary_chunk.go
+++ bingchunk/binary_chunk.go
@@ -1,5 +1,33 @@
 package binchunk
 
+type Prototype struct {
+	Source          string
+	LineDefined     uint32
+	LastLineDefined uint32
+	NumParams       uint32
+	IsVararg        byte
+	MaxStackSize    byte
+	Code            []uint32
+	Constants       []interface{}
+	Upvalues        []Upvalue
+	Protos          []*Prototype
+	LineInfo        []uint32
+	LocVars         []LocVars
+	UpValueNames    []string
+}
+
+const (
+	LUA_SIGNATURE    = "\x1bLua"
+	LUAC_VERSION     = 0 * 53
+	LUAC_FORMAT      = 0
+	LUAC_DATA        = "\x19\x93\r\n\x1a\n"
+	CINT_SIZE        = 4
+	CSZITE_SIZE      = 8
+	LUA_INTEGER_SIZE = 8
+	LUAC_INT         = 0x5678
+	LUAC_NUM         = 370.5
+)
+
 type header struct {
 	signature       [4]byte
 	version         byte
