de0cb3591396dd154c14c35647cb1100eec02b5f
Radium <2043869032@qq.com>
2023-01-11 23:18:12 +0800

    chore: commit

---
+++ state/api_stack.go
@@ -0,0 +1,54 @@
+package state
+
+func (self *luaState) GetTop() int {
+	return self.stack.top
+}
+
+func (self *luaState) AbsIndex(idx int) int {
+	return self.stack.absIndex(idx)
+}
+
+func (self *luaState) CheckStack(n int) bool {
+	self.stack.check(n)
+	return true
+}
+
+func (self *luaState) Pop(n int) {
+	for i := 0; i < n; i++ {
+		self.stack.pop()
+	}
+}
+
+func (self *luaState) Copy(formIdx, toIdx int) {
+	val := self.stack.get(formIdx)
+	self.stack.set(toIdx, val)
+}
+
+func (self *luaState) PushValue(idx int) {
+	val := self.stack.get(idx)
+	self.stack.push(val)
+}
+
+func (self *luaState) Replace(idx int) {
+	val := self.stack.pop()
+	self.stack.set(idx, val)
+}
+
+func (self *luaState) Insert(idx int) {
+	self.Rotate(idx, 1)
+	self.Pop(1)
+}
+
+func (self *luaState) Rotate(idx, n int) {
+	t := self.stack.top - 1
+	p := self.stack.absIndex(idx)
+	var m int
+	if n >= 0 {
+		m = t - n
+	} else {
+		m = p - n - 1
+	}
+	self.stack.reverse(p, m)
+	self.stack.reverse(m+1, t)
+	self.stack.reverse(p, t)
+}
