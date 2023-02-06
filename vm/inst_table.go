386bdf1b234d3a392427b600bcea420ffa7f12aa
2043869032@qq.com <2043869032@qq.com>
2023-02-06 20:35:41 +0800

    chore: commit

--- vm/inst_table.go
+++ vm/inst_table.go
@@ -8,3 +8,11 @@ func newTable(i Instruction, vm LuaVM) {
 	vm.CreateTable(Fb2int(b), Fb2int(c))
 	vm.Replace(a)
 }
+
+func getTable(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+	vm.GetRK(c)
+	vm.GetTable(b)
+	vm.Replace(a)
+}
