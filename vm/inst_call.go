cc185cd841d5c3f8bfc57b44a67ecb4d36d02b21
2043869032@qq.com <2043869032@qq.com>
2023-03-01 10:04:43 +0800

    chore: commit

--- vm/inst_call.go
+++ vm/inst_call.go
@@ -17,4 +17,31 @@ func call(i Instruction, vm LuaVM) {
 	a += 1
 
 	nArgs := _pushFuncAndArgs(a, b, vm)
+	vm.Call(nArgs, c-1)
+	_popResults(a, c, vm)
+}
+
+func _pushFuncAndArgs(a, b int, vm LuaVM) (nArgs int)  {
+	if b >= 1 {
+		vm.CheckStack(b)
+		for i:=a, i<a+b ; i++ {
+		 vm.pushValue(i)
+		}
+
+		return b - 1
+	}else {}
+}
+
+
+func _popResults(a, c int,vm  LuaVM) {
+	if c ==1 {
+
+	}else if c>1 {
+	   for i:=a+c-2;i>=a; i-- {
+		vm.Replace(i)
+	   }else {
+	    vm.CheckStack(1)
+		vm.PushInteger(int64(a))
+	   }
+	}
 }
